f78de1992ba5a88d3dd11179d554e645c1c6ec2f
Matheus Oliveira <matheus.felipee.oliveira@gmail.com>
2025-12-30 21:55:51 -0300

    fix: adjust NODE_ENV check to use development instead of dev

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,17 @@
 import cors from "@fastify/cors";
-import Fastify from "fastify"
-import type {FastifyInstance}  from "fastify";
+import Fastify, { type FastifyInstance } from "fastify";
 import { env } from "./config/env";
 import routes from "./routes";
 
 const app: FastifyInstance = Fastify({
   logger: {
-    level: env.NODE_ENV === "dev" ? "info" : "error",
+    level: env.NODE_ENV === "development" ? "info" : "error",
   },
 });
 
- app.register(cors, {
+app.register(cors, {
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   origin: true,
-  
 });
 
 app.register(routes, { prefix: "/api" });
